2e942484118dacae5e2ff5516f32c461c05db630
Lexx <spencermainaa@gmail.com>
2023-11-21 14:03:32 +0300

    feat: started on homepage. navbar, HomeHero, images. onClickOutside nav close

---
+++ src/components/max-width-wrapper.tsx
@@ -0,0 +1,22 @@
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+
+interface Props {
+  className?: string;
+  children: ReactNode;
+}
+
+const MaxWidthWrapper = async ({ children, className }: Props) => {
+  return (
+    <div
+      className={cn(
+        "mx-auto w-full max-w-screen-xl px-2.5 md:px-20",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default MaxWidthWrapper;
